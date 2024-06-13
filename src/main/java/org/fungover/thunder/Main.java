6f20a856b0a40ad96540509fda5c2618662c1c39
Linus Andreasson <lmmandreasson@gmail.com>
2024-06-13 15:46:42 +0200

    Enabled logging. Fixed merge
    Signed-off-by: Linus Andreasson <lmmandreasson@gmail.com>

--- src/main/java/org/fungover/thunder/Main.java
+++ src/main/java/org/fungover/thunder/Main.java
@@ -7,8 +7,6 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
-    private static final Logger logger = Logger.getLogger(Main.class.getName());
-
     public static void main(String[] args) {
         Server server;
         try {
